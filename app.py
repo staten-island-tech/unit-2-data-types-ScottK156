e25ea11dee98490294e3c06bbd821a9d05a024c7
ScottK156 <scottk8@nycstudents.net>
2025-02-05 09:24:21 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+""" #Tip and total calculator
+def add(x,y):
+    print(round(x + y))
+bill= float(input("How much was the bill"))
+print(bill)
+tip = int(bill * 0.15)
+print(round(tip))
+print(add(tip,bill)) """
+
+sentence = input("Make a sentence") 
+y = sentence.split()
+print(y)
+
+ 
