563da25854a9c3eaf8357ca3d48c2e38859f9442
ScottK156 <scottk8@nycstudents.net>
2025-02-06 09:23:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,23 @@ tip = int(bill * 0.15)
 print(round(tip))
 print(add(tip,bill)) """
 
-sentence = input("Make a sentence") 
-y = sentence.split()
-print(y)
+""" sentence = input("Make a sentence")  # creates an input where you type your sentence
+y = sentence.split() #turns the input into a list
+total= len(y)   #counts the amount of items in the list
+print("Total number of words in the sentence is",total) #prints the number of items in the list """
+""" #madlibs project
+print(f"I was *verb1* down the street. Then I saw a *noun*.Right next to *noun* I saw  *famous* they were *verb2* from *number* dog.")
+verb1 = input("Verb1")
+noun = input("Noun")
+famous = input("Famous Person")
+verb2 = input("Verb2")
+number = input("Number")
+if number == 1 :
+    dog = "dog" 
+else:
+    dog ="dogs"
+madlib = (f"I was {verb1} down the street. Then I saw a {noun}.Right next to {noun} I saw  {famous} they were {verb2} from {number} {dog}.")
+print(madlib) """
+number = input("Give me a munber")
+if number % 2 == 0
 
- 
