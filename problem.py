fd0c4bf1d351cdcb2ff1966bd87805a019b22417
ScottK156 <scottk8@nycstudents.net>
2025-03-04 09:25:25 -0500

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,20 +1,42 @@
-y = 1
 
-x = int(input("Give me a number"))
 
-while True:
+""" x = int(input("Give me a number")) """
+
+
+
+""" while True:    
+ x = int(input("Give me a number"))
  if x == 0:
-    print("Error give me another number")
-    x = int(input("Give me a number"))
+    print("Error")
+ elif x > 0:
+    for i in range(11):
+       z = x *  i
+       print(z)
+       if i == 11:
+          done = input("Would you like to continue or exit")
+ elif done.lower() == "exit":     
+    break
+ elif done.lower() == "continue":
+    continue """
+"""  elif x == 0:
+   print("Error")
+   x
+    
  elif x == "exit":
-     break
- for i in range(11):
-    z = y * x
-    print(z)
-    y += 1
-    if y == 11:
-      x = int(input("Give me a number"))
-      y = 1
+   break """
+while True: 
+    x = int(input("Enter a Number"))
+    if x == 0:
+        print("Error")
+    else :
+        for i in range(11):
+            print(x*i)
+    
+    
+
+
+
+
     
   
 
